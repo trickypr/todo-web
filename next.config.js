a83d894dc9195668bbb284c166c7a4c055ddd304
trickypr <trickypr@users.noreply.github.com>
2020-12-30 15:59:59 +1100

    :lipstick: Add google font

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const GoogleFontsPlugin = require('@beyonk/google-fonts-webpack-plugin')
+
 module.exports = (_, { defaultConfig }) => {
   return {
     async redirects() {
@@ -8,6 +10,18 @@ module.exports = (_, { defaultConfig }) => {
           permanent: true,
         },
       ]
+    },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      // Note: we provide webpack above so you should not `require` it
+      // Perform customizations to webpack config
+      config.plugins.push(
+        new GoogleFontsPlugin({
+          fonts: [{ family: 'Nunito', variants: ['400', '700'] }],
+        })
+      )
+
+      // Important: return the modified config
+      return config
     },
   }
 }
