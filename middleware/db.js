15414e7a3c42bcb39045c214b16c9f9beeaf6fb0
trickypr <trickypr@users.noreply.github.com>
2021-01-01 11:57:48 +1100

    :bug: Build uri at excecution rather than as an env variable

--- middleware/db.js
+++ middleware/db.js
@@ -10,10 +10,13 @@ global.mongo = global.mongo || {}
 
 const connectToDb = (handler) => async (req, res) => {
   if (!global.mongo.client) {
-    global.mongo.client = new MongoClient(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    global.mongo.client = new MongoClient(
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
     await global.mongo.client.connect()
   }
   req.dbClient = global.mongo.client
